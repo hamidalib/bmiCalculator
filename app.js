d17197e572a46df0af723ae98c8814feea822a5e
Hamid Ali <businesswithhamid@gmail.com>
2025-04-13 16:32:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let calcBtn = document.getElementById("calcBtn");
+
+calcBtn.addEventListener("click", function () {
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  const bmiResult = document.getElementById("resultNumber");
+  let resultBG = document.getElementById("resultCircle");
+  let bmiCat = document.getElementById("bmiCat");
+
+  if (height === "" || weight === "") {
+    alert("Please fill in all fields.");
+    return;
+  }
+  let result = (weight * 10000) / (height * height);
+
+  document.getElementById("tempText").style.display = "none";
+  resultCircle.classList.remove("hidden");
+  resultBox.classList.remove("justify-center");
+  resultNumber.classList.remove("hidden");
+  bmiCatText.classList.remove("hidden");
+  bmiCat.classList.remove("hidden");
+  bmiMeter.classList.remove("hidden");
+
+  bmiResult.innerHTML = result.toFixed(2);
+
+  if (result < 18.5) {
+    resultBG.style.backgroundColor = "#33C3F0";
+    bmiCat.innerHTML = "Underweight";
+    bmiCat.style.color = "#33C3F0";
+  } else if (result >= 18.5 && result < 24.9) {
+    resultBG.style.backgroundColor = "#4ADE80";
+    bmiCat.innerHTML = "Normal weight";
+    bmiCat.style.color = "#4ADE80";
+  } else if (result >= 25 && result < 29.9) {
+    resultBG.style.backgroundColor = "#FBBF24";
+    bmiCat.innerHTML = "Overweight";
+    bmiCat.style.color = "#FBBF24";
+  } else {
+    resultBG.style.backgroundColor = "#F87171";
+    bmiCat.innerHTML = "Obesity";
+    bmiCat.style.color = "#F87171";
+  }
+});
